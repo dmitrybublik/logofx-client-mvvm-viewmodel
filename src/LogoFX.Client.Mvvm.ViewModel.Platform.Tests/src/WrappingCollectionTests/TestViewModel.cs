88cfc96e58b846832c549979022a23f97deb53a9
godrose <godrose@gmail.com>
2016-03-15 19:34:01 +0200

    Added initial view model. Tests will be added after the model package is updated.

---
+++ src/LogoFX.Client.Mvvm.ViewModel.Platform.Tests/src/WrappingCollectionTests/TestViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LogoFX.Client.Mvvm.ViewModel.Tests.WrappingCollectionTests
+{
+    class TestViewModel
+    {
+        public TestModel Model { get; private set; }
+
+        public TestViewModel(TestModel model)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ src/LogoFX.Client.Mvvm.ViewModel.Platform.Tests/src/WrappingCollectionTests/WrappingCollectionTestsBase.cs
@@ -0,0 +1,26 @@
+using System.Windows.Threading;
+using LogoFX.Client.Tests.Infra;
+using NUnit.Framework;
+
+namespace LogoFX.Client.Mvvm.ViewModel.Tests.WrappingCollectionTests
+{
+    abstract class WrappingCollectionTestsBase
+    {
+        [SetUp]
+        protected void Setup()
+        {
+            SetupCore();
+            SetupOverride();
+        }
+
+        private void SetupCore()
+        {
+            Dispatch.Current = new SameThreadDispatch();
+        }
+
+        protected virtual void SetupOverride()
+        {
+            
+        }
+    }
+}
