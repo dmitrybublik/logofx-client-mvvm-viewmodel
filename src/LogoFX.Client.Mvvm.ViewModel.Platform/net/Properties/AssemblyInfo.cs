9a43585d84190c3ba8fb533acca21c9b319a1c12
Gennady Verdel <godrose@gmail.com>
2017-12-17 07:41:24 +0200

    Removed net45 projects #5

--- src/LogoFX.Client.Mvvm.ViewModel.Platform/net/Properties/AssemblyInfo.cs
+++ src/LogoFX.Client.Mvvm.ViewModel.Platform/net/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -12,7 +12,14 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e96cb49c-8aa8-4cbc-b143-4c4da11998f5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -24,5 +31,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.6")]
-[assembly: AssemblyFileVersion("1.0.6")]
+[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1")]
--- src/LogoFX.Client.Mvvm.ViewModel.Platform/src/WrappingCollectionExtensions.cs
+++ src/LogoFX.Client.Mvvm.ViewModel.Platform/src/WrappingCollectionExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections;
-#if NET45
+#if NET
 using System.Windows.Data;
 #endif
 
