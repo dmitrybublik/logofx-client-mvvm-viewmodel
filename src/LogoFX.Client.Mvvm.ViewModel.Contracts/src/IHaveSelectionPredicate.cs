6a771613758e0d95c59f4e7c21889e7cdb136034
Gennady Verdel <godrose@gmail.com>
2018-05-20 18:02:18 +0300

    One missing bit of documentation

--- src/LogoFX.Client.Mvvm.ViewModel.Contracts/src/IHaveSelectionPredicate.cs
+++ src/LogoFX.Client.Mvvm.ViewModel.Contracts/src/IHaveSelectionPredicate.cs
@@ -7,6 +7,9 @@ namespace LogoFX.Client.Mvvm.ViewModel.Contracts
     /// </summary>
     public interface IHaveSelectionPredicate
     {
+        /// <summary>
+        /// Gets or sets the selection predicate.
+        /// </summary>
         Predicate<object> SelectionPredicate { get; set; }
     }
 }
